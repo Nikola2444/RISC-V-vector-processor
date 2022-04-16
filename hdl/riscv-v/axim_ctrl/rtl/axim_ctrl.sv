78d3f6c1d02acc8ae61c9d30a3a0cbbb8c50fb0c
fouste <djordjemiseljic@gmail.com>
2022-04-17 00:00:04 +0200

    generalization of autogenerated axim_controller modules

--- hdl/riscv-v/axim_ctrl/rtl/axim_ctrl.sv
+++ hdl/riscv-v/axim_ctrl/rtl/axim_ctrl.sv
@@ -3,6 +3,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 // default_nettype of none prevents implicit wire declaration.
 `default_nettype none
+timeunit 1ps;
+timeprecision 1ps;
 
 module axim_ctrl #(
   parameter integer C_M_AXI_ADDR_WIDTH       = 64 ,
@@ -12,40 +14,44 @@ module axim_ctrl #(
 )
 (
   // System Signals
-  input wire                                    aclk               ,
-  input wire                                    areset             ,
-  // Extra clocks
-  input wire                                    kernel_clk         ,
-  input wire                                    kernel_rst         ,
+  input  wire                                   clk                ,
+  input  wire                                   rst                ,
   // AXI4 master interface
   output wire                                   m_axi_awvalid      ,
-  input wire                                    m_axi_awready      ,
+  input  wire                                   m_axi_awready      ,
   output wire [C_M_AXI_ADDR_WIDTH-1:0]          m_axi_awaddr       ,
   output wire [8-1:0]                           m_axi_awlen        ,
   output wire                                   m_axi_wvalid       ,
-  input wire                                    m_axi_wready       ,
+  input  wire                                   m_axi_wready       ,
   output wire [C_M_AXI_DATA_WIDTH-1:0]          m_axi_wdata        ,
   output wire [C_M_AXI_DATA_WIDTH/8-1:0]        m_axi_wstrb        ,
   output wire                                   m_axi_wlast        ,
   output wire                                   m_axi_arvalid      ,
-  input wire                                    m_axi_arready      ,
+  input  wire                                   m_axi_arready      ,
   output wire [C_M_AXI_ADDR_WIDTH-1:0]          m_axi_araddr       ,
   output wire [8-1:0]                           m_axi_arlen        ,
-  input wire                                    m_axi_rvalid       ,
+  input  wire                                   m_axi_rvalid       ,
   output wire                                   m_axi_rready       ,
-  input wire [C_M_AXI_DATA_WIDTH-1:0]           m_axi_rdata        ,
-  input wire                                    m_axi_rlast        ,
-  input wire                                    m_axi_bvalid       ,
+  input  wire [C_M_AXI_DATA_WIDTH-1:0]          m_axi_rdata        ,
+  input  wire                                   m_axi_rlast        ,
+  input  wire                                   m_axi_bvalid       ,
   output wire                                   m_axi_bready       ,
-  input wire                                    ap_start           ,
-  output wire                                   ap_done            ,
-  input wire [C_M_AXI_ADDR_WIDTH-1:0]           ctrl_addr_offset   ,
-  input wire [C_XFER_SIZE_WIDTH-1:0]            ctrl_xfer_size_in_bytes,
-  input wire [C_ADDER_BIT_WIDTH-1:0]            ctrl_constant
+  input  wire                                   ctrl_rstart        ,
+  output wire                                   ctrl_rdone         ,
+  input  wire [C_M_AXI_ADDR_WIDTH-1:0]          ctrl_raddr_offset  ,
+  input  wire [C_XFER_SIZE_WIDTH-1:0]           ctrl_rxfer_size    ,
+  output wire                                   rd_tvalid          ,
+  input  wire                                   rd_tready          ,
+  output wire                                   rd_tlast           ,
+  output wire [C_M_AXI_DATA_WIDTH-1:0]          rd_tdata           ,
+  input  wire                                   ctrl_wstart        ,
+  output wire                                   ctrl_wdone         ,
+  input  wire [C_M_AXI_ADDR_WIDTH-1:0]          ctrl_waddr_offset  ,
+  input  wire [C_XFER_SIZE_WIDTH-1:0]           ctrl_wxfer_size    ,
+  input  wire                                   wr_tvalid          ,
+  output wire                                   wr_tready          ,
+  input  wire [C_M_AXI_DATA_WIDTH-1:0]          wr_tdata
 );
-
-timeunit 1ps;
-timeprecision 1ps;
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -58,25 +64,6 @@ localparam integer LP_BRAM_DEPTH           = 512;
 localparam integer LP_RD_MAX_OUTSTANDING   = LP_BRAM_DEPTH / LP_AXI_BURST_LEN;
 localparam integer LP_WR_MAX_OUTSTANDING   = 32;
 
-///////////////////////////////////////////////////////////////////////////////
-// Wires and Variables
-///////////////////////////////////////////////////////////////////////////////
-
-// Control logic
-logic                          done = 1'b0;
-// AXI read master stage
-logic                          read_done;
-logic                          rd_tvalid;
-logic                          rd_tready;
-logic                          rd_tlast;
-logic [C_M_AXI_DATA_WIDTH-1:0] rd_tdata;
-// Adder stage
-logic                          adder_tvalid;
-logic                          adder_tready;
-logic [C_M_AXI_DATA_WIDTH-1:0] adder_tdata;
-
-// AXI write master stage
-logic                          write_done;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Begin RTL
@@ -91,12 +78,12 @@ axim_ctrl_axi_read_master #(
   .C_INCLUDE_DATA_FIFO ( 1                     )
 )
 inst_axi_read_master (
-  .aclk                    ( aclk                    ) ,
-  .areset                  ( areset                  ) ,
-  .ctrl_start              ( ap_start                ) ,
-  .ctrl_done               ( read_done               ) ,
-  .ctrl_addr_offset        ( ctrl_addr_offset        ) ,
-  .ctrl_xfer_size_in_bytes ( ctrl_xfer_size_in_bytes ) ,
+  .aclk                    ( clk                     ) ,
+  .areset                  ( rst                     ) ,
+  .ctrl_start              ( ctrl_rstart         		 ) ,
+  .ctrl_done               ( ctrl_rdone              ) ,
+  .ctrl_addr_offset        ( ctrl_raddr_offset       ) ,
+  .ctrl_xfer_size_in_bytes ( ctrl_rxfer_size 		     ) ,
   .m_axi_arvalid           ( m_axi_arvalid           ) ,
   .m_axi_arready           ( m_axi_arready           ) ,
   .m_axi_araddr            ( m_axi_araddr            ) ,
@@ -105,37 +92,14 @@ inst_axi_read_master (
   .m_axi_rready            ( m_axi_rready            ) ,
   .m_axi_rdata             ( m_axi_rdata             ) ,
   .m_axi_rlast             ( m_axi_rlast             ) ,
-  .m_axis_aclk             ( kernel_clk              ) ,
-  .m_axis_areset           ( kernel_rst              ) ,
+  .m_axis_aclk             ( clk                     ) ,
+  .m_axis_areset           ( rst                     ) ,
   .m_axis_tvalid           ( rd_tvalid               ) ,
   .m_axis_tready           ( rd_tready               ) ,
   .m_axis_tlast            ( rd_tlast                ) ,
   .m_axis_tdata            ( rd_tdata                )
 );
 
-/* DUMMY
-axim_ctrl_adder #(
-  .C_AXIS_TDATA_WIDTH ( C_M_AXI_DATA_WIDTH ) ,
-  .C_ADDER_BIT_WIDTH  ( C_ADDER_BIT_WIDTH  ) ,
-  .C_NUM_CLOCKS       ( 1                  )
-)
-inst_adder  (
-  .s_axis_aclk   ( kernel_clk                   ) ,
-  .s_axis_areset ( kernel_rst                   ) ,
-  .ctrl_constant ( ctrl_constant                ) ,
-  .s_axis_tvalid ( rd_tvalid                    ) ,
-  .s_axis_tready ( rd_tready                    ) ,
-  .s_axis_tdata  ( rd_tdata                     ) ,
-  .s_axis_tkeep  ( {C_M_AXI_DATA_WIDTH/8{1'b1}} ) ,
-  .s_axis_tlast  ( rd_tlast                     ) ,
-  .m_axis_aclk   ( kernel_clk                   ) ,
-  .m_axis_tvalid ( adder_tvalid                 ) ,
-  .m_axis_tready ( adder_tready                 ) ,
-  .m_axis_tdata  ( adder_tdata                  ) ,
-  .m_axis_tkeep  (                              ) , // Not used
-  .m_axis_tlast  (                              )   // Not used
-);
-DUMMY*/ 
 
 // AXI4 Write Master
 axim_ctrl_axi_write_master #(
@@ -146,12 +110,12 @@ axim_ctrl_axi_write_master #(
   .C_INCLUDE_DATA_FIFO ( 1                     )
 )
 inst_axi_write_master (
-  .aclk                    ( aclk                    ) ,
-  .areset                  ( areset                  ) ,
-  .ctrl_start              ( ap_start                ) ,
-  .ctrl_done               ( write_done              ) ,
-  .ctrl_addr_offset        ( ctrl_addr_offset        ) ,
-  .ctrl_xfer_size_in_bytes ( ctrl_xfer_size_in_bytes ) ,
+  .aclk                    ( clk                     ) ,
+  .areset                  ( rst                     ) ,
+  .ctrl_start              ( ctrl_wstart             ) ,
+  .ctrl_done               ( ctrl_wdone              ) ,
+  .ctrl_addr_offset        ( ctrl_waddr_offset       ) ,
+  .ctrl_xfer_size_in_bytes ( ctrl_wxfer_size         ) ,
   .m_axi_awvalid           ( m_axi_awvalid           ) ,
   .m_axi_awready           ( m_axi_awready           ) ,
   .m_axi_awaddr            ( m_axi_awaddr            ) ,
@@ -163,15 +127,14 @@ inst_axi_write_master (
   .m_axi_wlast             ( m_axi_wlast             ) ,
   .m_axi_bvalid            ( m_axi_bvalid            ) ,
   .m_axi_bready            ( m_axi_bready            ) ,
-  .s_axis_aclk             ( kernel_clk              ) ,
-  .s_axis_areset           ( kernel_rst              ) ,
-  .s_axis_tvalid           ( adder_tvalid            ) ,
-  .s_axis_tready           ( adder_tready            ) ,
-  .s_axis_tdata            ( adder_tdata             )
+  .s_axis_aclk             ( clk                     ) ,
+  .s_axis_areset           ( rst                     ) ,
+  .s_axis_tvalid           ( wr_tvalid               ) ,
+  .s_axis_tready           ( wr_tready               ) ,
+  .s_axis_tdata            ( wr_tdata                )
 );
 
-assign ap_done = write_done;
 
-endmodule : axim_ctrl
+ endmodule : axim_ctrl
 `default_nettype wire
 

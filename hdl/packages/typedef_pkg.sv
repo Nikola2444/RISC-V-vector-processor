bbd6a8b47e03a2cfa3348c542b1fa0fc43b70f28
nikola2444 <nikola2three4@gmail.com>
2022-04-26 09:43:19 +0200

    added alu_vld_i and alu_vld_o in alu.sv unit

--- hdl/packages/typedef_pkg.sv
+++ hdl/packages/typedef_pkg.sv
@@ -1,5 +1,5 @@
-`ifndef ENUM_PKG
-`define ENUM_PKG
+`ifndef TYPEDEF_ENUM_PKG
+`define TYPEDEF_ENUM_PKG
 
 typedef enum logic [6:0] {// add sub
 			  add_op=7'b000000, sub_op=7'b000011,
@@ -17,6 +17,16 @@ typedef enum logic [6:0] {// add sub
 			  sll_op=7'b1100000, srl_op=7'b1100001, sra_op=7'b1100010, nsrl_op=7'b1100011, 
 			  nsra_op=7'b1100100
 			  } alu_op;
+
+typedef enum logic [10:0] {OPMVV_101xxx_rdy=0, OPMVX_101xxx_rdy=1,
+			   STORE_IDX_rdy=2,STORE_rdy=3, LOAD_IDX_rdy=4,
+			   LOAD_rdy=5, OPMVV_rdy=6, OPMVX_rdy=7,
+			   OPIVV_rdy=8, OPIVI_rdy=9, OPIVX_rdy=10} instruction_rdy;
+
+typedef enum logic [10:0] {OPMVV_101xxx_vld=11'b00000000001, OPMVX_101xxx_vld=11'b00000000010,
+			   STORE_IDX_vld=11'b00000000100,STORE_vld=11'b00000001000, LOAD_IDX_vld=11'b00000010000,
+			   LOAD_vld=11'b00000100000, OPMVV_vld=11'b00001000000, OPMVX_vld=11'b00010000000,
+			   OPIVV_vld=11'b00100000000, OPIVI_vld=11'b01000000000, OPIVX_vld=11'b10000000000} instruction_vld;
 
 
  
--- hdl/riscv-v/vector_core/v_lane/rtl/alu.sv
+++ hdl/riscv-v/vector_core/v_lane/rtl/alu.sv
@@ -8,22 +8,25 @@ module alu #
     input 				       clk,
     input 				       rstn,
 
-    // Interfaces towards DSPs in ALU. If alu_en_32bit_mul==1
+    // Interfaces towards DSPs in ALU. If alu_en_32bit_mul_i==1
     // then only [0] interface is used
-    input [PARALLEL_IF_NUM-1:0][6:0] 	       alu_opmode,
+    input [PARALLEL_IF_NUM-1:0][6:0] 	       alu_opmode_i,
     input [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0]  alu_a_i,
     input [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0]  alu_b_i,
     input [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0]  alu_c_i,
-    input [1:0] 			       sew,
+    input [1:0] 			       sew_i,
     output [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0] alu_o,
+    input [PARALLEL_IF_NUM-1:0] 	       alu_vld_i,
+    output [PARALLEL_IF_NUM-1:0] 	       alu_vld_o,
 
     // Enables 32 bit multiply, but takes a bit longer to
     // execute.
-    input 				       alu_en_32bit_mul,
+    input 				       alu_en_32bit_mul_i,
     // stalls all registers in ALU
-    input 				       alu_stall
+    input 				       alu_stall_i
     );
-   
+
+   localparam MAX_PIPE_STAGES=3;
    logic [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0]   dsp_a_reg;
    logic [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0]   dsp_b_reg;
    logic [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0]   dsp_c_reg;
@@ -31,7 +34,7 @@ module alu #
    logic [PARALLEL_IF_NUM-1:0][2*OP_WIDTH-1:0] dspu_m_reg;
    logic [PARALLEL_IF_NUM-1:0][2*OP_WIDTH-1:0] dspsu_m_reg;
    logic [PARALLEL_IF_NUM-1:0][OP_WIDTH-1:0]   dsp_p_reg;
-
+   logic [MAX_PIPE_STAGES-1:0] 		       res_vld_reg;
 
    always_ff@(posedge clk)
    begin
@@ -45,7 +48,7 @@ module alu #
       end
       else
       begin
-	 if (!alu_stall)
+	 if (!alu_stall_i)
 	   for (int i=0; i<PARALLEL_IF_NUM; i++)
 	   begin
 	      dsp_a_reg[i] <= alu_a_i;
@@ -54,7 +57,7 @@ module alu #
 	      dsp_m_reg[i] <= signed'(dsp_a_reg) * signed'(dsp_b_reg);
 	      dspu_m_reg[i] <= unsigned'(dsp_a_reg) * unsigned'(dsp_b_reg);
 	      dspsu_m_reg[i] <= signed'(dsp_a_reg) * unsigned'(dsp_b_reg);
-	      case (alu_opmode[i])
+	      case (alu_opmode_i[i])
 		 add_op: dsp_p_reg[i] 	 <= dsp_a_reg[i] + dsp_b_reg[i];
 		 sub_op: dsp_p_reg[i] 	 <= dsp_a_reg[i] - dsp_b_reg[i];
 		 xor_op: dsp_p_reg[i] 	 <= dsp_a_reg[i] ^ dsp_b_reg[i];
@@ -69,24 +72,37 @@ module alu #
 		 sltu_op: dsp_p_reg[i] 	 <= unsigned'(dsp_a_reg[i]) < unsigned'(dsp_b_reg[i]) ? 32'b1 : 32'b0;
 		 sgtu_op: dsp_p_reg[i] 	 <= unsigned'(dsp_a_reg[i]) > unsigned'(dsp_b_reg[i]) ? 32'b1 : 32'b0;
 		 mul_op: dsp_p_reg[i] 	 <= dsp_m_reg[i][31:0];
-		 mulhu_op: dsp_p_reg[i]  <= sew == 2'b00 ? dspu_m_reg[i][7:0]:
-					    sew == 2'b01 ? dspu_m_reg[i][15:0]:
+		 mulhu_op: dsp_p_reg[i]  <= sew_i == 2'b00 ? dspu_m_reg[i][7:0]:
+					    sew_i == 2'b01 ? dspu_m_reg[i][15:0]:
 					    dspu_m_reg[i][63:32];
-		 mulhsu_op: dsp_p_reg[i] <= sew == 2'b00 ? dspsu_m_reg[i][7:0]:
-					    sew  == 2'b01 ? dspsu_m_reg[i][15:0]:
+		 mulhsu_op: dsp_p_reg[i] <= sew_i == 2'b00 ? dspsu_m_reg[i][7:0]:
+					    sew_i  == 2'b01 ? dspsu_m_reg[i][15:0]:
 					    dspsu_m_reg[i][63:32];
 		 mulh_op: dsp_p_reg[i] 	 <= dsp_m_reg[i][31:0];
 
 		 wmul_op: dsp_p_reg[i] 	 <= dsp_m_reg[i][31:0];
 		 wmulu_op: dsp_p_reg[i]  <= dspu_m_reg[i][31:0];
 		 wmulsu_op: dsp_p_reg[i] <= dspsu_m_reg[i][31:0];
-
-		 //Finish shifting
-		 
+		 //Finish shifting		 
 	      endcase
 	   end
       end
    end
+
+   always @ (posedge clk)
+   begin
+      if (!rstn)
+      begin
+	 res_vld_reg <= '{default:'0};
+      end
+      else
+      begin
+	 res_vld_reg <= {res_vld_reg[MAX_PIPE_STAGES-2:0], alu_vld_i};
+      end
+   end
+
+   
+   assign alu_vld_o = alu_opmode_i[6]==1'b1 ? res_vld_reg[MAX_PIPE_STAGES-1]:res_vld_reg[MAX_PIPE_STAGES-2];
 
    generate
       for (genvar i=0; i<PARALLEL_IF_NUM;i++)

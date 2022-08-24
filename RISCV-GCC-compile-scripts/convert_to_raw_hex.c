35a22613a800342eaae546de5df783cd5b6c34c8
fouste <djordjemiseljic@gmail.com>
2022-08-24 14:19:58 +0200

    sritp for making raw assembly and .h added

---
+++ RISCV-GCC-compile-scripts/convert_to_raw_hex.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char assembly_instr[32];
+int main(void) {
+    const char* ifilename = "assembly_noaddr.dump";
+    const char* ofilename = "assembly_raw.dump";
+    const char* hfilename = "assembly.h";
+
+    FILE* input_file  = fopen(ifilename, "r");
+    if (!input_file)
+        exit(EXIT_FAILURE);
+    FILE* output_file = fopen(ofilename, "w");
+    if (!output_file)
+        exit(EXIT_FAILURE);
+    FILE* header_file = fopen(hfilename, "w");
+    if (!header_file)
+        exit(EXIT_FAILURE);
+
+    char *contents = NULL;
+    size_t num = 0;
+    size_t len = 0;
+    fprintf(header_file," static int assembly [] = { \n");
+    while (getline(&contents, &len, input_file) != -1)
+    {
+        
+        //printf("Line=%s", contents);
+        num=-1;
+        num = sscanf(contents," %[0-9a-f] ",assembly_instr);
+        if(num>0 && strlen(assembly_instr)==8) 
+        {
+          fprintf(output_file,"%s\n", assembly_instr);
+          fprintf(header_file,"0x%s,\n", assembly_instr);
+          //printf("Assembly=%s\n", assembly_instr);
+        }
+    }
+
+    fprintf(header_file,"0x00000013\n}");
+    fclose(input_file);
+    free(contents);
+
+    exit(EXIT_SUCCESS);
+}

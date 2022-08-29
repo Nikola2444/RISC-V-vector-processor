3b5be3298fd26e1da5f88c2c41e80dc61020e754
fouste <djordjemiseljic@gmail.com>
2022-08-29 13:28:58 +0200

    3x3 coded needs doublechecking

--- Software/3x3conv_layer.s
+++ Software/3x3conv_layer.s
@@ -15,23 +15,23 @@ li x21, 1024     # Pointer to start of image
 li x22, 1048576  # Pointer to start of weights
 li x23, 1064960  # Pointer to start of results
 
-addi x6,x21,x0  # Pointer to start of image
-addi x2,x22,x0  # Pointer to start of weights
-addi x3,x23,x0  # Pointer to start of results
-addi x5,x10,x0  # Set to track number of pixels
+addi x6, x11, x0  # Iterator over filters 
+
+addi x2, x22,x0  # Pointer to start of weights
+
+vsetvli x1, x12, e8, m1	# 8-bit data
+
+l_ld_next_filter: nop
 
 addi x24, x21, 0    # First row
 add  x25, x24, x15  # Second row
 add  x26, x25, x15  # Third row
 
-addi x27, x24, 0 #First row and counting
-addi x28, x25, 0 #Second row and counting
-addi x29, x26, 0 #Third row and counting
+addi x5,  x10, x0  # Iterator over X
+addi x4,  x9,  x0  # Iterator over Y
 
-vmul.vx       v31, v31, 0     # Reset results
-vsetvli x31, x12, e8, m1	# 8-bit data
+add x3, x23, x0 # set start address if a result
 
-l_ld_next_filter: nop
 # LOAD A FILTER
 vle8.v  v10, (x2)
 add x2, x2, x12
@@ -51,6 +51,14 @@ vle8.v  v17, (x2)
 add x2, x2, x12
 vle8.v  v18, (x2)
 add x2, x2, x12
+
+l_start_next_row: nop
+
+addi x27, x24, 0 #First row and counting
+addi x28, x25, 0 #Second row and counting
+addi x29, x26, 0 #Third row and counting
+
+vmul.vx       v31, v31, 0     # Reset results
 
 #LOAD FIRST PIXEL IN A ROW
 #First row
@@ -96,26 +104,30 @@ vredsum.vs    v31, v29, v31	  # sum to zeroth
 vmul.vv       v29, v8,  v18  # Multiply weights and pixels
 vredsum.vs    v31, v29, v31	  # sum to zeroth 
 
-vslideup.vx   v30, v29, x30		# slide up result by one each time 
-vadd.vx       v31, v30, x0		# mov to result vector
+vsetvli       x1, x30, e8, m1	# 8-bit data
+vse8.v        v31, (x3)          # load image in v0
+add           x3, x3, x11
+vsetvli       x1, x12, e8, m1	# 8-bit data
+
+addi x5, x5, -1
 
 l_next_3_same_row: nop
 
 # Load another 3 pixels, shift to left the ones being reused
-addi x27, x27, 12
 vadd.vx       v0, v1, x0
 vadd.vx       v1, v2, x0
 vle8.v        v2, (x27)
+add x27, x27, x12
 
-addi x28, x28, 12
 vadd.vx       v3, v4, x0
 vadd.vx       v4, v5, x0
 vle8.v        v5, (x28)
+add x28, x28, x12
 
-addi x29, x29, 12
 vadd.vx       v6, v7, x0
 vadd.vx       v7, v8, x0
 vle8.v        v8, (x29)
+add x29, x29, x12
 
 # MAC FILTER X IMAGE 3x3
 vmul.vx       v30, v30, 0     # Reset results
@@ -138,8 +150,46 @@ vredsum.vs    v31, v29, v31	  # sum to zeroth
 vmul.vv       v29, v8,  v18  # Multiply weights and pixels
 vredsum.vs    v31, v29, v31	  # sum to zeroth 
 
-vslideup.vx   v30, v29, x30		# slide up result by one each time 
-vadd.vx       v31, v30, x0		# mov to result vector
+vsetvli       x1, x30, e8, m1	# 8-bit data
+vse8.v        v31, (x3)          # load image in v0
+add           x3, x3, x11
+vsetvli       x1, x12, e8, m1	# 8-bit data
+
+addi x5, x5, -1
+
+beq x5, x0, l_ld_row_finished
+
+jal l_next_3_same_row
+
+l_row_finished: nop
+
+#go to next row
+
+addi x4, x4, -1
+
+add x24, x25, x0 
+add x25, x26, x0
+add x26, x26, x15
+
+beq x4, x0, l_ld_filter_finished
+
+jal l_start_next_row
+
+l_ld_filter_finished: nop
+
+addi x23, x23, 1 # results incremented by 1 for next filter
+addi x6, x6 -1
+
+beq x6, x0, l_finised
+
+jal l_ld_next_filter
+
+l_finished: nop
+nop
+nop
+nop
+
+
 
 
 

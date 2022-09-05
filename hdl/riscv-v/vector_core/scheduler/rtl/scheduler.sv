349ce29faa95cbca15b9b521f498d2f8688d2034
fouste <djordjemiseljic@gmail.com>
2022-09-05 11:49:22 +0200

    indedntation fix

--- hdl/riscv-v/vector_core/scheduler/rtl/scheduler.sv
+++ hdl/riscv-v/vector_core/scheduler/rtl/scheduler.sv
@@ -66,7 +66,7 @@ module scheduler
    logic [5:0] 	       v_instr_funct6;
    logic [1:0] 	       v_instr_mop;   
 
-   logic 	       next_instr_rdy;
+   logic 	             next_instr_rdy;
 
    logic               mcu_ld_buffering_reg;
    logic               mcu_ld_buffered_reg;
@@ -84,34 +84,33 @@ module scheduler
    assign mcu_data_width_o = vector_instr_reg[14:12];
 
 
-   always @(posedge clk)
-   begin
+    always @(posedge clk)
+    begin
       if (!rstn)
       begin
-	 vector_instr_reg  <= 'h0;
-	 scalar_rs1_reg    <= 'h0;
-	 scalar_rs2_reg    <= 'h0;
+        vector_instr_reg  <= 'h0;
+        scalar_rs1_reg    <= 'h0;
+        scalar_rs2_reg    <= 'h0;
       end
       else if (next_instr_rdy)
       begin
-	 vector_instr_reg  <= vector_instr_next;
-	 scalar_rs1_reg    <= rs1_i;
-	 scalar_rs2_reg    <= rs2_i;
+        vector_instr_reg  <= vector_instr_next;
+        scalar_rs1_reg    <= rs1_i;
+        scalar_rs2_reg    <= rs2_i;
       end
-   end
+    end
 
-   always @(posedge clk)
-   begin
+    always @(posedge clk)
+    begin
       if (!rstn)
       begin
-	 v_OPCFG_check_reg <= 1'b0;
+        v_OPCFG_check_reg <= 1'b0;
       end
       else
       begin
-	 v_OPCFG_check_reg <= v_OPCFG_check;
+        v_OPCFG_check_reg <= v_OPCFG_check;
       end
-   end
-
+    end
    
 
    //if the instructions is indexed load, for the next instruction we insert the second part of that instruction which is
@@ -124,20 +123,21 @@ module scheduler
    
    //combinational logic bellow checks the opcode field of an
    //vector instruction
-   always_comb
-   begin
+    always_comb
+    begin
       v_st_instr_check = 1'b0;
       v_ld_instr_check = 1'b0;
       v_arith_instr_check = 1'b0;
       if (v_instr_opcode == v_st_opcode)
-	v_st_instr_check = 1'b1;
+        v_st_instr_check = 1'b1;
       if (v_instr_opcode == v_ld_opcode)
-	v_ld_instr_check = 1'b1;
+        v_ld_instr_check = 1'b1;
       if (v_instr_opcode == v_arith_opcode)
-	v_arith_instr_check = 1'b1;     
-   end
-   always_comb
-   begin
+        v_arith_instr_check = 1'b1;     
+    end
+
+    always_comb
+    begin
       v_OPIVV_check = 1'b0;
       v_OPIVI_check = 1'b0;
       v_OPIVX_check = 1'b0;
@@ -148,48 +148,48 @@ module scheduler
       v_OPCFG_check = 1'b0;
 
       if (v_instr_funct3==OPIVV)
-	v_OPIVV_check = 1'b1;
+        v_OPIVV_check = 1'b1;
       if (v_instr_funct3==OPIVI)
-	v_OPIVI_check = 1'b1;
+        v_OPIVI_check = 1'b1;
       if (v_instr_funct3==OPIVX)
-	v_OPIVX_check = 1'b1;
+        v_OPIVX_check = 1'b1;
       if (v_instr_funct3==OPMVV)
-	v_OPMVV_check = 1'b1;
+        v_OPMVV_check = 1'b1;
       if (v_instr_funct3==OPMVX)
-	v_OPMVX_check = 1'b1;
+        v_OPMVX_check = 1'b1;
       if (v_instr_funct3==OPFVV)
-	v_OPFVV_check = 1'b1;
+        v_OPFVV_check = 1'b1;
       if (v_instr_funct3==OPFVF)
-	v_OPFVF_check = 1'b1;
+        v_OPFVF_check = 1'b1;
       if (v_instr_funct3==OPCFG)
-	v_OPCFG_check = 1'b1;
-   end
+        v_OPCFG_check = 1'b1;
+    end
 
    // combinational logic bellow checks mop field of a
    // vector instruction
-   always_comb
-   begin
+    always_comb
+    begin
       v_unit_check = 1'b0;
       v_strided_check = 1'b0;
       v_idx_unordered_check = 1'b0;
       v_idx_ordered_check = 1'b0;
-      
 
       if (v_instr_mop == unit_stride)
       begin
-	 v_unit_check = 1'b1;
+        v_unit_check = 1'b1;
       end
       if (v_instr_mop == strided)
       begin
-	 v_strided_check = 1'b1;
+        v_strided_check = 1'b1;
       end
       if (v_instr_mop == idx_unordered)
       begin
-	 v_idx_unordered_check = 1'b1;
+        v_idx_unordered_check = 1'b1;
       end
-      if (v_instr_mop == idx_ordered)
-	v_idx_ordered_check = 1'b1;
-   end
+      if (v_instr_mop == idx_ordered) begin
+        v_idx_ordered_check = 1'b1;
+      end
+    end
 
    assign 	 mcu_unit_ld_st_o = v_unit_check;
    assign 	 mcu_idx_ld_st_o = v_idx_unordered_check;
@@ -197,84 +197,87 @@ module scheduler
    //combinational logic bellow checks funct3 field of an 
    //vector instruction
    
-   always_comb
-   begin
+    always_comb
+    begin
       instr_vld_o = 'h0;
-      
+      // Store instruction
       if (v_st_instr_check)
       begin
-	 if ((v_strided_check || v_unit_check) && mcu_st_rdy_i)
-	   instr_vld_o = STORE_vld;
-	 else if ((v_idx_ordered_check || v_idx_unordered_check) && mcu_st_rdy_i)
-	   instr_vld_o = STORE_IDX_vld;
+        if ((v_strided_check || v_unit_check) && mcu_st_rdy_i)
+          instr_vld_o = STORE_vld;
+        else if ((v_idx_ordered_check || v_idx_unordered_check) && mcu_st_rdy_i)
+          instr_vld_o = STORE_IDX_vld;
       end
+      // Load instruction
       if (v_ld_instr_check)
       begin
-	 if ((v_strided_check || v_unit_check) && mcu_ld_buffered_reg)
-	   instr_vld_o = LOAD_vld;
-	 else if ((v_idx_ordered_check || v_idx_unordered_check))
-	   instr_vld_o = LOAD_IDX_vld;	
+        if ((v_strided_check || v_unit_check) && mcu_ld_buffered_reg)
+          instr_vld_o = LOAD_vld;
+        else if ((v_idx_ordered_check || v_idx_unordered_check))
+          instr_vld_o = LOAD_IDX_vld;	
       end
+      // Arithmetic instruction
       if (v_arith_instr_check)
       begin
-	 if (v_OPIVV_check)
-	   instr_vld_o = OPIVV_vld;
-	 if (v_OPIVI_check)
-	   instr_vld_o = OPIVI_vld;
-	 if (v_OPIVX_check)
-	   instr_vld_o = OPIVX_vld;
-	 if (v_OPMVV_check)
-	   if (v_instr_funct6_upper==3'b101)
-	     instr_vld_o = OPMVV_101xxx_vld;
-	   else
-	     instr_vld_o = OPMVV_vld;
-	 if (v_OPMVX_check)
-	   if (v_instr_funct6_upper==3'b101)
-	     instr_vld_o = OPMVX_101xxx_vld;
-	   else
-	     instr_vld_o = OPMVX_vld;
-	 if (v_OPCFG_check)
-	   instr_vld_o = OPCFG_vld;
-	 if (v_instr_funct6==6'b001110 || v_instr_funct6==6'b001111)
-	   instr_vld_o = SLIDE_vld;
+        if (v_OPIVV_check)
+          instr_vld_o = OPIVV_vld;
+        if (v_OPIVI_check)
+          instr_vld_o = OPIVI_vld;
+        if (v_OPIVX_check)
+          instr_vld_o = OPIVX_vld;
+        if (v_OPMVV_check)
+        begin
+          if (v_instr_funct6_upper==3'b101)
+           instr_vld_o = OPMVV_101xxx_vld;
+          else
+           instr_vld_o = OPMVV_vld;
+        end
+        if (v_OPMVX_check)
+        if (v_instr_funct6_upper==3'b101)
+         instr_vld_o = OPMVX_101xxx_vld;
+        else
+         instr_vld_o = OPMVX_vld;
+        if (v_OPCFG_check)
+          instr_vld_o = OPCFG_vld;
+        if (v_instr_funct6==6'b001110 || v_instr_funct6==6'b001111)
+          instr_vld_o = SLIDE_vld;
       end            
-   end
+    end
 
    //logic that checks if there is a load being buffered by M_CU
-   assign mcu_ld_vld_o = (!mcu_ld_buffering_reg && !mcu_ld_buffered_reg && v_ld_instr_check && !v_OPCFG_check_reg);
-   always @(posedge clk)
-   begin
+    assign mcu_ld_vld_o = (!mcu_ld_buffering_reg && !mcu_ld_buffered_reg && v_ld_instr_check && !v_OPCFG_check_reg);
+    always @(posedge clk)
+    begin
       if (!rstn)
       begin
-	 mcu_ld_buffering_reg <= 'h0;
+        mcu_ld_buffering_reg <= 'h0;
       end
       else
       begin
-	 if (mcu_ld_vld_o && mcu_ld_rdy_i && !mcu_ld_buffering_reg)
-	 begin
-	    mcu_ld_buffering_reg <= 1'b1;
-	 end
-	 else if (mcu_ld_buffered_i)
-	   mcu_ld_buffering_reg <= 1'b0;
-      end
-   end
+        if (mcu_ld_vld_o && mcu_ld_rdy_i && !mcu_ld_buffering_reg)
+        begin
+          mcu_ld_buffering_reg <= 1'b1;
+        end
+        else if (mcu_ld_buffered_i)
+          mcu_ld_buffering_reg <= 1'b0;
+        end
+    end
 
-   always @(posedge clk)
-   begin
+    always @(posedge clk)
+    begin
       if(!rstn)begin
-	 mcu_ld_buffered_reg <=0;
+        mcu_ld_buffered_reg <=0;
       end
       else
-	if (!mcu_ld_buffered_reg)
-	  mcu_ld_buffered_reg <= mcu_ld_buffered_i;
-	else if (instr_rdy_i[5:4]!=0)
-	  mcu_ld_buffered_reg <= 0;
-   end
+      if (!mcu_ld_buffered_reg)
+        mcu_ld_buffered_reg <= mcu_ld_buffered_i;
+      else if (instr_rdy_i[5:4]!=0)
+        mcu_ld_buffered_reg <= 0;
+    end
 
    assign mcu_st_vld_o = v_st_instr_check && !v_idx_unordered_check && instr_rdy_i[3:2]!=0;
 
    //check handshake between scheduler and V_CU
-   
    assign next_instr_rdy = ((instr_vld_o & instr_rdy_i) != 'h0 || instr_vld_o == 0) && 
 			   !(v_ld_instr_check && !mcu_ld_buffered_reg) &&
 			   !(mcu_st_vld_o && !mcu_st_rdy_i);
@@ -282,8 +285,6 @@ module scheduler
    // maybe extented
    // Stall if new instructions is valid but v_cu is not ready,
    // if we have a store instruction but m_cu is not ready,
-   // 
-
    assign vector_stall_o   = (!next_instr_rdy && (v_st_instr_check | v_ld_instr_check | v_arith_instr_check)) | 
 			     (mcu_st_vld_o && !mcu_st_rdy_i) | 
 			     (v_ld_instr_check && !mcu_ld_buffered_reg);
